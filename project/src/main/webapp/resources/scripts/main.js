4d8d4f409280f4399be68e600631c519081dabb6
Steve <stephen.pepple@gmail.com>
2013-03-26 16:54:27 -0700

    Move logic from HTML to JS.

--- project/src/main/webapp/resources/scripts/main.js
+++ project/src/main/webapp/resources/scripts/main.js
@@ -1,15 +1,23 @@
 /* Constants */
 CITY_CENTER = {
-  sf : new google.maps.LatLng(37.7750, -122.4183),
+  sf : new google.maps.LatLng(37.7670, -122.4383),
   geneva : new google.maps.LatLng(46.2000, 6.1500),
   zurich : new google.maps.LatLng(47.3690, 8.5380)
 }
 
+pink = "#E07C94";
+orange = "#E48E3B";
+green = "#B8CF81";
+blue = "#9EC5D9";
+yellow = "#FCD116"
+
 DATE_FORMAT = d3.time.format("%b %Y");
+/* TODO: Allow any Day */
+DAY = "10/01/2012"
 
 /* Globals */
 window.sf = new Object;
-window.sf.abbr = [{"1":["001","01EXTRA"],"2":"002","3":"002-WKY","4":"002-WKY","5":"005","6":"006","7":"006","71":"006","71L":"006","8X":"008X","9":"009","9L":"009","12":["012-WKY","012"],"14":"014","16X":"016X","17":"017","18":"018","19":["019","019-WKY"],"21":"021","22":"022","23":"023","24":"024","27":"027","28":["028","028 SAT","028-WKY"],"29":"029","30":"030","30X":"130X","31":"031","33":"033","35":"035","36":"036","37":"037","38":"038","39":"039","41":"041","43":["043","043-WKY"],"44":"044","45":"030","47":["047-WKY","047"],"48":"048","49":"049","52":"052","54":"054","56":"056","59":"059","61":"61","66":"66","67":"67","80":"080","80X":"080","81X":"080","82X":"080","90":"090","91":"091","14L":"114XL","14X":"114XL","95":"095SD","38AX":"138L","38BX":"138L","1X":"101X","31X":"101X","38X":"101X", "L-OWL": "094 MC L-N OWL", "M-OWL": "094 MC L-N OWL", "N-OWL": "094 MC L-N OWL", "38L":"138L", "J":"093", "M":"093", "N":"093"}];
+window.sf.abbr = [{"1":["001","01EXTRA"],"2":"002","3":"002-WKY","4":"002-WKY","5":"005","6":"006","7":"006","71":"006","71L":"006","8X":"008X","9":"009","9L":"009","12":["012-WKY","012"],"88":"038","14":"014","16X":"016X","17":"017","18":"018","19":["019","019-WKY"],"21":"021","22":"022","23":"023","24":"024","27":"027","28":["028","028 SAT","028-WKY"],"29":"029","30":"030","30X":"130X","31":"031","33":"033","35":"035","36":"036","37":"037","38":"038","39":"039","41":"041","43":["043","043-WKY"],"44":"044","45":"030","47":["047-WKY","047"],"48":"048","49":"049","52":"052","54":"054","56":"056","59":"059","61":"61","66":"66","67":"67","80":"080","80X":"080","81X":"080","82X":"080","90":"090","91":"091","14L":"114XL","14X":"114XL","95":"095SD","38AX":"138L","38BX":"138L","1X":"101X","31X":"101X","38X":"101X", "L-OWL": "094 MC L-N OWL", "M-OWL": "094 MC L-N OWL", "N-OWL": "094 MC L-N OWL", "38L":"138L", "J":"093", "M":"093", "N":"093"}];
 window.sf.routes = new Array();
     
 // more styles: http://mapsys.info/34436/styled-maps-using-google-maps-api-version-3/ 
@@ -20,76 +28,79 @@ var simple = [{"featureType":"road","elementType":"geometry","stylers":[{"visibi
 var simple2 = [{"featureType":"road","elementType":"geometry","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"landscape","elementType":"labels","stylers":[{"visibility":"off"}]}]
 var buildings = [{"featureType":"administrative","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"landscape.natural","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"off"}]}]
 
-var open = [ { "featureType": "administrative", "stylers": [ { "visibility": "off" } ] },{ "featureType": "landscape", "stylers": [ { "visibility": "simplified" }, { "color": "#fafafa" } ] },{ "featureType": "poi", "stylers": [ { "visibility": "off" } ] },{ "featureType": "road.highway", "stylers": [ { "visibility": "off" } ] },{ "featureType": "road.local", "elementType": "geometry", "stylers": [ { "visibility": "on" }, { "lightness": 30 } ] },{ "featureType": "administrative", "elementType": "labels", "stylers": [ { "visibility": "off" } ] },{ "featureType": "road", "elementType": "labels", "stylers": [ { "visibility": "off" } ] },{ "featureType": "road.arterial", "elementType": "geometry.stroke", "stylers": [ { "visibility": "on" }, { "color": "#dcdcdc" } ] },{ "featureType": "transit", "stylers": [ { "visibility": "off" } ] },{ "elementType": "labels", "stylers": [ { "visibility": "off" } ] },{ "featureType": "road", "elementType": "geometry.stroke", "stylers": [ { "visibility": "on" }, { "weight": NaN }, { "color": "#ebebeb" } ] },{ "featureType": "road", "elementType": "geometry.fill", "stylers": [ { "color": "#fafafa" } ] }, { "featureType": "road.highway", "stylers": [ { "visibility": "off" } ] } ]
-
+var white = [ { "featureType": "administrative", "stylers": [ { "visibility": "off" } ] },{ "featureType": "landscape", "stylers": [ { "visibility": "simplified" }, { "color": "#fafafa" } ] },{ "featureType": "poi", "stylers": [ { "visibility": "off" } ] },{ "featureType": "road.highway", "stylers": [ { "visibility": "off" } ] },{ "featureType": "road.local", "elementType": "geometry", "stylers": [ { "visibility": "on" }, { "lightness": 30 } ] },{ "featureType": "administrative", "elementType": "labels", "stylers": [ { "visibility": "off" } ] },{ "featureType": "road", "elementType": "labels", "stylers": [ { "visibility": "off" } ] },{ "featureType": "road.arterial", "elementType": "geometry.stroke", "stylers": [ { "visibility": "on" }, { "color": "#dcdcdc" } ] },{ "featureType": "transit", "stylers": [ { "visibility": "off" } ] },{ "elementType": "labels", "stylers": [ { "visibility": "off" } ] },{ "featureType": "road", "elementType": "geometry.stroke", "stylers": [ { "visibility": "on" }, { "weight": NaN }, { "color": "#ebebeb" } ] },{ "featureType": "road", "elementType": "geometry.fill", "stylers": [ { "color": "#fafafa" } ] }, { "featureType": "road.highway", "stylers": [ { "visibility": "off" }, { "featureType": "water", "elementType": "geometry.stroke", "stylers": [ { "visibility": "on" }, { "weight": 1 } ] }] } ]
+var open = [
+  {
+    "stylers": [
+      { "visibility": "off" }
+    ]
+  },{
+    "featureType": "water",
+    "elementType": "geometry.fill",
+    "stylers": [
+      { "visibility": "on" },
+      { "color": "#DDDDDD" }
+    ]
+  },{
+    "featureType": "road",
+    "elementType": "geometry",
+    "stylers": [
+      { "visibility": "on" },
+      { "color": "#999999" },
+      { "weight": 0.2 }
+    ]
+  },{
+    "featureType": "landscape",
+    "elementType": "geometry.fill",
+    "stylers": [
+      { "visibility": "on" },
+      { "color": "#646464" }
+    ]
+  },{
+  }
+]
 /* Setup the Google Map Enviornment */
-var map;
-var city = $("#city").val();
+var map, city;
 
 window.stops = null;
 window.routes = null;
 window.paths = null;
+window.circleOptions = {};
     
 function initialize() {
+	
+	city = $("#city").val();
   
-  var mapOptions = {
-    center: CITY_CENTER[city],
-    zoom: 13,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
+	var mapOptions = {
+		center: CITY_CENTER[city],
+		zoom: 13,
+		disableDefaultUI: true,
+		zoomControl: true,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
 
-  map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
-
-  // http://gmaps-samples-v3.googlecode.com/svn/trunk/styledmaps/wizard/index.html
-  map.setOptions({styles: open});
-  /* Show the transit layer
-   * var transitLayer = new google.maps.TransitLayer();
-   * transitLayer.setMap(map); 
-   */
+	// http://gmaps-samples-v3.googlecode.com/svn/trunk/styledmaps/wizard/index.html
+	map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+	map.setOptions({styles: open});
+  
+	loadRoutes(citySelect.val());    
+  
 }    
     
 /* Populate the List of Routes */
 var routeSelect = $("#route");
 var googleOptions = { strokeColor: '#00746b', strokeWeight: 2 };
 var colors = ["00308F", "A32638", "841B2D", "3D0C02", "004225", "00563F", "232B2B", "013220", "004B49"]
-
-var circleOptions = {
-    strokeColor: "#FF0000",
-    strokeOpacity: 0.8,
-    strokeWeight: 2,
-    fillColor: "#FF0000",
-    fillOpacity: 0.35,
-    map: map,
-    center: CITY_CENTER[city],
-    radius: 10
-};
-
-/* Listen for City Change */
-citySelect = $("#city");
-loadRoutes(citySelect.val());
-    
-citySelect.bind("change", function(){
-  var value = $(this).val();
-      
-  loadRoutes(value);
-      
-  /* Load the new city center */
-  google.maps.event.trigger(map, 'resize');
-  map.setCenter(CITY_CENTER[value]);
-});
         
-/* 
- * Routes.csv is just a list of routes, not the shape files 
- */
+/* Routes.csv is just a list of routes, not the shape files */
 function loadRoutes(city) {
-
   var path = "resources/data/";
 
   d3.csv(path + city + "/routes/routes.csv", function(data) {
         
     window.routes = data;
+    /* Don't show routes selector
     routeSelect.empty().append("<option>Select a route ...</option>");
-
     data.forEach(function(d, i){
       if (city == "sf") {
         var value = d.route_short_name + " -- " + d.route_long_name + "  " + d.route_id;
@@ -104,189 +115,135 @@ function loadRoutes(city) {
         routeSelect.append($("<option></option>").attr("value",d.route_short_name).text(value)); 
       }
     });
-        
+    */
+
     /* Routes.csv is the geoJSON files */
     $.getJSON(path + city + '/routes/routes.json', function(data) {
               
       var myGoogleVector = new GeoJSON(data, googleOptions);
       window.paths = myGoogleVector;
-          
-      routeSelect.bind("change", function(){
-        var route = $(this).val();
 
-        showRoute(route);
-            
+      /* For now, chart everything */
+      chartJSON()      
+    });
+  });      
+}
+
+/* Typical run is .408 ms: 
+*/
+function loadData(city) {
+    var bus;
+
+    /* TODO: Reduce size before downloading */
+    d3.csv("resources/data/sf/bus-capacity.csv", function(buses) {
+
+      var formatNumber = d3.format(",d");
+        formatChange = d3.format("+,d");
+        formatDate = d3.time.format("%B %d, %Y");
+        formatTime = d3.time.format("H:%M:%S");
+
+      var nestByDate = d3.nest().key(function(d) { return d3.time.day(d.date); });
+
+	  /* ~ 60ms; 16ms w/o time func */
+      buses.forEach(function(d, i) {
+        d.index = i;
+		/* Native JS date actually seems to be faster 
+        d.time = d3.time.format(d.time); */
+        d.time = new Date(DAY + " " + d.time);
       });
-    });
-  });
+
+      bus = crossfilter(buses)
     
-  // Populate the List of Stops
-  var stopSelect = $("#stops");
+      var all = bus.groupAll();
+
+      // dim by trip
+      var trip = bus.dimension(function(d) { return d.trip_id });
+      var trips = trip.group();      
+
+      var time = bus.dimension(function(d) { return d.time });
+      var times = time.group();
+
+      var min = new Date(DAY + " 06:00:00");
+      var max = new Date(DAY + " 22:00:00");
+
+      var date_range = Math.abs(min - max);
+      var minutes = date_range / 1000 / 60;
       
-  d3.csv(path + city + "/stops/stops.csv", function(data) {
-    window.stops = data;
+      /* Add date segments to the slider */
+      var segments = minutes / 60;
+      
+      for (var i=0; i < segments; i++) {
+        var increment = (i * 60) + 30;
         
-    stopSelect.empty().append("<option>Select a stop ...</option>");
+        var label = new Date(min.getTime() + (increment * 1000 * 60));
         
-    data.forEach(function(d, i){
-      var value = d.stop_name + " -- " + d.stop_id;
-      stopSelect.append($("<option></option>").attr("value", d.stop_id).text(value)); 
-    });
-  });
-    
-  stopSelect.bind("change", function(){
-    var value = $(this).val();
-
-    var stop = window.stops.filter(function(d) {
-      return d.stop_id == value;
-    });
-              
-    var coord = new google.maps.LatLng(stop[0].stop_lat, stop[0].stop_lon);
-                
-    map.addMarker(coord);
-      
-    $("#info .stop").html(stop[0].stop_name)
-    google.maps.event.trigger(map, 'resize');
-    map.setCenter(coord);
-      
-  });
-      
-}
-
-function removeRoutes() {
-  /* TODO: Fix this
-   * Remove Routes w/o Paths */        
-  routeSelect.find("option[value='10']").remove();
-  routeSelect.find("option[value='8AX']").remove();
-  routeSelect.find("option[value='BAX']").remove();
-  routeSelect.find("option[value='28L']").remove();
-  routeSelect.find("option[value='76']").remove();
-  routeSelect.find("option[value='88']").remove();
-}
-    
-function showRoute(route) {
-  if (city == "sf") { var routeName = window.sf.abbr[0][route]; }
-
-  
-  map.clearMarkers();
-  
-
-  /* Check the Depth of the geoJSON 
-     TODO: Move to reusable func.
-     TODO: SANJU to Add his d3 geoJSON, which is colored by route. 
-     TODO: Add or remove route based upon the route select box. 
-  */
-  var path = new Array();
-  var length = 0;
-            
-	for (var i = 0; i < window.paths.length; i++){
-            
-    var color = colors[i % colors.length];
-
-		if(window.paths[i].length){
-                
-			for(var j = 0; j < window.paths[i].length; j++){
-        var line = window.paths[i][j].geojsonProperties.LINEABBR;
-        $("#paths").append()
+        var width = $("#slider").innerWidth();
+        var labelWidth = (100 / segments)
         
-        //console.log(window.paths[0][0])
+        var segment = $("<div class='segment'</div>");
+        
+        segment.append("<div class='tick center'></div><span class='label'>" + getDateLabel(label) + "</span>");
+        segment.css("width", labelWidth + "%");
+        
+        
+        $("#range").append(segment);
+      };
+      
+      /* Trigger first Filter */
+      filterByTime(min, new Date(min.getTime() + (1 * (60 + 5))));
+      $("#currentTime").html( getDateTicker(min, true) );
+
+      $("#slider").slider({
+        value: 5,
+        min: 0,
+        max: minutes,
+        step: 5,
+        slide: function(event, ui) {
+
+          var val = ui.value;
           
-        if (city == "sf") {
-          if (routeName[name] == line || routeName.indexOf(line) > -1) {
-            window.paths[i][j].strokeColor = "#" + color;
-            window.paths[i][j].strokeWeight = 1;
-  					window.paths[i][j].setMap(map);
-            length += google.maps.geometry.spherical.computeLength(window.paths[i][j].getPath()) / 1000;
-          } else {
-            window.paths[i][j].setMap(null);
-          }
-        } else if (city == "geneva") {
-          window.paths[i][j].strokeColor = "#" + color;
-          window.paths[i][j].strokeWeight = 1;
-					window.paths[i][j].setMap(map);
-          length += google.maps.geometry.spherical.computeLength(window.paths[i][j].getPath()) / 1000;
+          $("#currentTime").html( getDateTicker(new Date(min.getTime() + (val * 1000 * 60)), true));
+        },
+        change: function( event, ui ) {
+
+          map.clearOverlays();
+
+          var val = ui.value;
           
-          if (window.paths[i][j].geojsonProperties.routeCode == route || window.paths[i][j].geojsonProperties.route_id == route) {
+          var start = new Date(min.getTime() + (val * 1000 * 60));
+          /* add 30 minutes */
+          var end = new Date(start.getTime() + (5  * 1000 * 60));
           
-          } else {
-            window.paths[i][j];
-            window.paths[i][j].setMap(null);
-          }
-          
-        }  
-			}
-		} else{
-      //console.log(window.paths[i])
-      //console.log(window.paths[i])
-      window.paths[i].strokeColor = "#" + color;
-			window.paths[i].setMap(map);
-                
-      //length = google.maps.geometry.spherical.computeLength(window.paths[i].getPath()) / 1000;
-                
-		}
-	
-  }
-  $.getJSON('http://localhost:8080/urban-data-challenge/rest/stop-service/sf/' + window.sf.routes[route] + '/null/4015', function(data) {
- 
-    var first, last = null;
-    
-    $.each(data, function(key, val) {
-      //  (key, val)
-      if (key == "coordinates") {
-        $.each(data.coordinates, function(key2, val2){
-          /*
-          val.split("\,");
-          var lat = val[0];
-          var lon = val[1];
-          */
-          //console.log(val2);
-          var latLon = val2.split(",");
-          
-          var coord = new google.maps.LatLng(latLon[0], latLon[1]);
-          
-          if (key2 == 0) { first = coord; }
-          if (key2 == data.length) { last = coord; }
-          
-          map.addMultipleMarkers(coord);
-                
+          filterByTime(start, end)
+
+        }
+      });
+      
+      $( "#amount" ).val( "$" + $( "#slider" ).slider( "value" ) );
+
+      function filterByTime(start, end) {
+
+        time.filterRange([start, end]);
+
+        time.top(Infinity).forEach(function(p, i){
+            var lat = p.stop_lat;
+            var lon = p.stop_lon;
+            var route = p.route
+
+            map.addOverlay(lat, lon, 150 * p.capacity)
+
+            //showRoute(route)
         });
       }
+      
+      /*
+      trip.filter("4116537-05");
+
+      trip.top(10).forEach(function(p, i){
+        console.log(p, i)
+      })
+      */
+
     });
-    
-    google.maps.event.trigger(map, 'resize');
-    
-    map.setCenter(first);
-    map.setZoom(14);
-    /* TODO: Check for inbound or outbound */
-    var bounds = new google.maps.LatLngBounds(first, last);
-    map.panToBounds(bounds)    
-           
-    
-  });
-  console.log(length)
 }
-
-/*
-var arrivals;
-    
-d3.csv("sf-route-1-schedule-real.csv", function(data) {
-
-  //console.log(routes)
-  arrivals = data;
-      
-  // TODO: Update to match arrival and schedule column
-  var formatNumber = d3.format(",d"),
-      formatChange = d3.format("+,d"),
-      formatDate = d3.time.format("%B %d, %Y"),
-      formatTime = d3.time.format("%I:%M %p");
-      
-  arrivals.forEach(function(d, i) {
-    d.index = i;
-    console.log(d.date)
-    //d.date = parseDate(d.date);
-    //d.delay = +d.delay;
-    //d.distance = +d.distance;
-  });
-                  
-});
-*/
+  
--- project/src/main/webapp/resources/scripts/utils.js
+++ project/src/main/webapp/resources/scripts/utils.js
@@ -6,7 +6,42 @@ function parseDate(d) {
       d.substring(4, 6),
       d.substring(6, 8));
 }
-    
+
+function getDateTicker(date, useHTML) {
+          
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var seconds = date.getSeconds();
+
+  /* Pad Values */
+  if (hours < 10) { hours = "0" + hours; }
+  if (minutes < 10) { minutes = "0" + minutes; }
+  if (seconds < 10) { seconds = "0" + seconds; }
+  
+  var string;
+  if (useHTML) {
+    string = $("<div class='hours'>" + hours + "</div><div>:</div><div class='minutes'>" + minutes + "</div><div>:</div><div class='seconds'>" + seconds + "</div>")
+  } else {
+    string = hours + ":" + minutes + ":" + seconds;
+  }
+  return string;
+}
+
+function getDateLabel(date) {
+          
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+
+  /* Pad Values */
+  if (hours < 10) { hours = "0" + hours; }
+  if (minutes < 10) { minutes = "0" + minutes; }
+  
+  string = hours + ":" + minutes;
+  return string;
+}
+
+
+/* JSON */    
 function filterJSON(json, key, value) {
   var result = {};
   for (var explosionIndex in json) {
@@ -16,9 +51,41 @@ function filterJSON(json, key, value) {
   }
   return result;
 }
+
+function chartJSON() {
+  color = "#EEEEEE"
+
+  var path = new Array();
+  var length = 0;
+            
+  for (var i = 0; i < window.paths.length; i++){
+            
+    if(window.paths[i].length){
+      for(var j = 0; j < window.paths[i].length; j++){
+        var line = window.paths[i][j].geojsonProperties.LINEABBR;
+        $("#paths").append()
+          
+        if (city == "geneva" || city == "sf") {
+          window.paths[i][j].strokeColor = color;
+          window.paths[i][j].strokeWeight = .5;
+          window.paths[i][j].setMap(map);
+          
+
+          window.paths[i][j].setMap(map);
+
+          
+        }  
+      }
+    } else{
+      window.paths[i].strokeColor = "#" + color;
+      window.paths[i].setMap(map);
+    }
+  }
+}
     
 /* Extend Google Maps */
 var markersArray = [];
+var overlays = [];
 google.maps.Map.prototype.addMarker = function(location) {
   map.clearMarkers();
       
@@ -39,11 +106,35 @@ google.maps.Map.prototype.addMultipleMarkers = function(location) {
       
   markersArray.push(marker);
 };
-
     
 google.maps.Map.prototype.clearMarkers = function() {
     for(var i=0; i < markersArray.length; i++){
         markersArray[i].setMap(null);
     }
     this.markers = new Array();
+};
+
+google.maps.Map.prototype.clearOverlays = function() {
+  for (var i = 0; i < overlays.length; i++ ) {
+    overlays[i].setMap(null);
+  }
+}
+
+google.maps.Map.prototype.addOverlay = function(lat, lon, size) {
+  var coord = new google.maps.LatLng(lat, lon)
+
+  var circleOptions = {
+    strokeColor: yellow,
+    strokeOpacity: 0.8,
+    strokeWeight: .8,
+    fillColor: yellow,
+    fillOpacity: 0.2,
+    map: map,
+    center: coord,
+    radius: size
+  };
+
+  var circle = new google.maps.Circle(circleOptions);
+      
+  overlays.push(circle);
 };
